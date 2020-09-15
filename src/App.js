c8456c5cc75c028dcb6b75f8070798acefe37ec1
Ali <shabazali.ali@siemens.com>
2020-09-15 06:17:11 +0530

    added how to track a logged-in  user, sign in,  sign out functionality and display logged in user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Header from'./Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Checkout from './Checkout';
 import Login from './Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //will only run once the app component loads
+    auth.onAuthStateChanged((authuser) => {
+      console.log("The use is ==> ",authuser);
+
+      if (authuser) {
+        //the use just logged in / the user was logged initialState
+        dispatch({
+          type: 'SET_USER',
+          user: authuser
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
   return (
     <Router>
       <div className="app">
